c8607d5c2b72ebe5ce862958954075d7b331a214
Jonatas Freire <jonatasf1997@hotmail.com>
2019-11-17 19:21:15 -0300

    Features: create migration plans

--- src/database/migrations/20191117220836-create-plans.js
+++ src/database/migrations/20191117220836-create-plans.js
@@ -1,30 +1,21 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('students', {
+    return queryInterface.createTable('plans', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
       },
-      name: {
+      title: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      dateage: {
-        type: Sequelize.DATEONLY,
+      duration: {
+        type: Sequelize.INTEGER,
         allowNull: false,
       },
-      weight: {
-        type: Sequelize.FLOAT,
-        allowNull: false,
-      },
-      height: {
+      price: {
         type: Sequelize.FLOAT,
         allowNull: false,
       },
@@ -40,6 +31,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('students');
+    return queryInterface.dropTable('plans');
   },
 };
