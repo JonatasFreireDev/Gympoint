1b62d14ad47edfb6d309c360814e107c8edfb9a1
Jonatas Freire <jonatasf1997@hotmail.com>
2019-12-10 20:41:09 -0300

    features: Queue and others

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 import './database';
@@ -12,6 +13,9 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      express.static(path.resolve(__dirname, 'app', 'views', 'image'))
+    );
   }
 
   routes() {
