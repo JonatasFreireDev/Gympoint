9f8e2a0cd37f6bd8d4b9f32113a971cdd805a790
Jonatas Freire <jonatasf1997@hotmail.com>
2019-12-22 15:21:10 -0300

    feature: add sentry

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
+import Youch from 'youch';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 import routes from './routes';
 
 import './database';
@@ -7,11 +11,14 @@ import './database';
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       express.static(path.resolve(__dirname, 'app', 'views', 'image'))
@@ -19,7 +26,19 @@ class App {
   }
 
   routes() {
+    this.server.use(Sentry.Handlers.errorHandler());
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
